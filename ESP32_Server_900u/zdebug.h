5185c7f39bcdd5f473f52fbc96abc7c81fdaadce
OpenGG <liy099@gmail.com>
2022-03-11 17:37:09 +0800

    feat: remove setup() delay for production build

--- ESP32_Server_900u/zdebug.h
+++ ESP32_Server_900u/zdebug.h
@@ -5,7 +5,9 @@
 #ifdef Z_DEBUG_ENABLE
 #define zdebugBegin(p) Serial.begin(p);
 #define zdebug(...) Serial.println(__VA_ARGS__);
+#define zdebugDelay(dur) delay(dur);
 #else
 #define zdebugBegin(p)
 #define zdebug(...)
+#define zdebugDelay(dur)
 #endif
