7caa02669a0e89bf1e9d396ab8406168b2fe619f
OpenGG <liy099@gmail.com>
2022-03-14 20:47:10 +0800

    feat: release

--- scripts/build-client-img.sh
+++ scripts/build-client-img.sh
@@ -4,5 +4,13 @@ set -e
 
 ./scripts/build-client.sh
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    mklittlefs=$(ls $HOME/Library/Arduino15/packages/esp32/tools/mklittlefs/*/mklittlefs)
+else
+
+    mklittlefs=$(ls $HOME/.arduino15/packages/esp32/tools/mklittlefs/*/mklittlefs)
+fi
+
 # fs size of 0x170000
-python3 scripts/mklfs.py -t 1507328
+$mklittlefs -c "./data" -b 4096 -p 1024 -s 1507328 lfs.out
