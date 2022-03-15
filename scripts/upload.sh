0d73090f08e089292ee4240e1eb27722a0ea3bad
OpenGG <liy099@gmail.com>
2022-03-15 16:14:41 +0800

    fix: esp32s2 build

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# ~/Library/Arduino15/packages/esp32/tools/esptool_py/3.1.0/esptool -p /dev/cu.usbserial-14230 -b 115200 --before default_reset --after hard_reset --chip ESP32-S2 write_flash --flash_mode qio --flash_size detect --flash_freq 80m 0x1000 ESP32_Server_900u.ino.bootloader.bin 0x8000 ESP32_Server_900u.ino.partitions.bin 0x10000 ESP32_Server_900u.ino.bin
-
-# ~/Library/Arduino15/packages/esp32/tools/esptool_py/3.1.0/esptool -p /dev/cu.usbmodem01 -b 115200 --before default_reset --after hard_reset --chip ESP32-S2 read_flash 0 0x400000 out.bin
-
-# ~/Library/Arduino15/packages/esp32/tools/esptool_py/3.1.0/esptool -p /dev/cu.usbserial-14230 -b 115200 --before default_reset --after hard_reset --chip ESP32-S2 read_flash --flash_mode qio --flash_size 4MB --flash_freq 80m 0 0x400000 out.bin
-
 # upload files
 
 set -xe
